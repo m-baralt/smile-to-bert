b10dd85ea3fe1283b453472b2a2871985555ba2e
maria <maria.barranco@upc.edu>
2024-10-29 15:46:23 +0000

    modified download smiles

--- download_smiles.sh
+++ download_smiles.sh
@@ -1,4 +1,4 @@
 mkdir -p smiles
 
-wget https://github.com/m-baralt/smile-to-bert/releases/download/v5.0/smiles_strings.txt -O smiles/smiles_strings.txt
+wget https://github.com/m-baralt/smile-to-bert/releases/download/v5.0/smiles_string.txt -O smiles/smiles_string.txt
 
